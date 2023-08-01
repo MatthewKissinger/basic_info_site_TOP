b384fdce79405a31e4afe5c9abd2f1a5eb62cf38
Matt <matthew.t.kissinger@gmail.com>
2023-07-31 17:36:52 -0700

    updated to using express

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-const http = require('http');
 const path = require('path');
-const fs = require('fs');
 
-http.createServer((req, res) => {
-    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url + '.html');
-    console.log(filePath);
+const express = require('express');
+const app = express();
 
-    // Read File
-    fs.readFile(filePath, (err, content) => {
-        if(err) {
-            if(err.code == 'ENOENT') {
-                // Page not found
-                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf-8');
-                })
-            } else {
-                // Server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
-            // Success
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf-8');
-        }
-    })
-}).listen(8080);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000);
